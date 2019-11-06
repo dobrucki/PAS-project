6be389b62d26c9a8403ba0d7d81100c5e14cdfc0
Jedrzej Dobrucki <dobrucki2@gmail.com>
2019-11-06 17:26:32 +0100

    Add Client Model class
    Removed Models/.gitkeep file.

---
+++ PAS-project/Models/Client.cs
@@ -0,0 +1,15 @@
+namespace PAS_project.Models
+{
+    public abstract class Client
+    {
+        public int Id { get; set; }
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Sex { get; set; }
+        
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
