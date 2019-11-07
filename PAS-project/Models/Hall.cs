8206c068d4bae5c6e1dd1fbf0ba15224df0c7bac
Jedrzej Dobrucki <dobrucki2@gmail.com>
2019-11-07 15:25:39 +0100

    Update Hall model class

--- PAS-project/Models/Hall.cs
+++ PAS-project/Models/Hall.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+
 namespace PAS_project.Models
 {
     public class Hall
     {
+        public int Number { get; set; }
+        private List<Seat> _seatsList;
+        
+        public Hall()
+        {
+            _seatsList = new List<Seat>();
+        }
         
     }
-}
+}   
