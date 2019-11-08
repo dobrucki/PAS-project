2fb0d77f6402ae38c1edf22b59b6180d65cb000e
Mateusz <wasilewskimateusz1998@gmail.com>
2019-11-08 13:13:59 +0100

    Add class MovieRepository

--- PAS-project/Models/Movie.cs
+++ PAS-project/Models/Movie.cs
@@ -5,5 +5,11 @@ namespace PAS_project.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+
+        public Movie(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
     }
 }
---
+++ PAS-project/Models/MovieRepository.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PAS_project.Models
+{
+    public class MovieRepository : IDataRepository<Movie>
+    {
+    private readonly List<Movie> _movies = new List<Movie>();
+
+    public void Add(Movie item)
+    {
+        if (item is null) throw new Exception("Given item is null");
+        if (_movies.Any(movie => movie.Id == item.Id)) throw new Exception("Given movie already exists!");
+        _movies.Add(item);
+    }
+
+    public Movie Get(int id)
+    {
+        var result = _movies.First(movie => movie.Id == id);
+        if (result is null) throw new Exception("Given id does not match any movie!");
+        return result;
+    }
+
+    public void Update(Movie item, int id)
+    {
+        if (item is null) throw new Exception("Given movie is null!");
+        try
+        {
+            var index = _movies.FindIndex(movie => movie.Id == id);
+            _movies[index] = item;
+        }
+        catch (ArgumentNullException)
+        {
+            throw new Exception("Given id does not match any movie!");
+        }
+    }
+
+    public void Remove(int id)
+    {
+        _movies.Remove(Get(id));
+    }
+
+    public void Remove(Movie item)
+    {
+        _movies.Remove(item);
+    }
+    }
+}
