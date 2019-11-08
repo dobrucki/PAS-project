7c4be5dbd2df4acb2097429c1189cf8935e1e8ba
Jedrzej Dobrucki <dobrucki2@gmail.com>
2019-11-08 10:02:32 +0100

    Add ClientRepository Model

---
+++ PAS-project/Models/ClientRepository.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PAS_project.Models
+{
+    public class ClientRepository : IDataRepository<Client>
+    {
+        private readonly List<Client> _clients = new List<Client>();
+        
+        public void Add(Client item)
+        {
+            if(_clients.Any(client => client.Id == item.Id)) throw new Exception("Given client already exists!");
+            _clients.Add(item);
+        }
+
+        public Client Get(int id)
+        {
+            var result = _clients.First(client => client.Id == id);
+            if (result is null) throw new Exception("Given id does not match any client!");
+            return result;
+        }
+
+        public void Update(Client item, int id)
+        {
+            try
+            {
+                var index = _clients.FindIndex(client => client.Id == id);
+                _clients[index] = item;
+            }
+            catch (ArgumentNullException)
+            {
+                throw new Exception("Given id does not match any client!");
+            }
+        }
+
+        public void Remove(int id)
+        {
+            _clients.Remove(Get(id));
+        }
+
+        public void Remove(Client item)
+        {
+            _clients.Remove(item);
+        }
+    }
+}
