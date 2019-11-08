7d14d42e319d9ed2b2ba320168adbe1eb45afa03
Jedrzej Dobrucki <dobrucki2@gmail.com>
2019-11-08 11:23:49 +0100

    Add Movie model class

---
+++ PAS-project/Models/Movie.cs
@@ -0,0 +1,10 @@
+namespace PAS_project.Models
+{
+    public class Movie : IModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
