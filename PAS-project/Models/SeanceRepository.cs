d3593e2b67d908893d4236fd188c12ab16924c38
Mateusz <wasilewskimateusz1998@gmail.com>
2019-11-12 00:44:14 +0100

    Add methods in SeanceRepository class

--- PAS-project/Models/SeanceRepository.cs
+++ PAS-project/Models/SeanceRepository.cs
@@ -1,32 +1,51 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PAS_project.Models
 {
     public class SeanceRepository : IDataRepository<Seance>
     {
-        public Seance Add(Seance item)
+        private readonly List<Seance> _seances = new List<Seance>();
+        
+        public Seance Add(Seance seance)
         {
-            throw new System.NotImplementedException();
+            var id = _seances.Count == 0 ? 1 : _seances.Last().Id+1;
+            seance.Id = id;
+            _seances.Add(seance);
+            return seance;
         }
 
         public Seance Get(int id)
         {
-            throw new System.NotImplementedException();
+            var result = _seances.First(seance => seance.Id == id);
+            if (result is null) throw new Exception("Given id does not match any seance!");
+            return result;
         }
 
         public IEnumerable<Seance> GetAll()
         {
-            throw new System.NotImplementedException();
+            return _seances;
         }
 
-        public Seance Update(Seance item)
+        public Seance Update(Seance updatedSeance)
         {
-            throw new System.NotImplementedException();
+            if (updatedSeance is null) throw new Exception("Given seance is null!");
+            var actualSeance = _seances.FirstOrDefault(m => m.Id == updatedSeance.Id);
+            if (actualSeance is null) throw new Exception("Given id does not match any seance!");
+            actualSeance.Movie = updatedSeance.Movie;
+            actualSeance.DurationTime = updatedSeance.DurationTime;
+            actualSeance.ScreeningRoom = updatedSeance.ScreeningRoom;
+            actualSeance.SeanceDateTime = updatedSeance.SeanceDateTime;
+            return actualSeance;
         }
 
         public Seance Delete(int id)
         {
-            throw new System.NotImplementedException();
+            var foundSeance = _seances.FirstOrDefault(c => c.Id == id);
+            if(foundSeance is null) throw new Exception("Given id does not match any seance!");
+            _seances.Remove(foundSeance);
+            return foundSeance;
         }
     }
 }
