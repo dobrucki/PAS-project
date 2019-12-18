2778f613014089780ea3eef23b8aaf7fce525d4a
Jedrzej Dobrucki <dobrucki2@gmail.com>
2019-12-18 15:30:51 +0100

    Update AddSeanceViewModel
    Added attributes in order to change default labels in adding form.

--- PAS-project/ViewModels/AddSeanceViewModel.cs
+++ PAS-project/ViewModels/AddSeanceViewModel.cs
@@ -5,9 +5,12 @@ namespace PAS_project.ViewModels
     public class AddSeanceViewModel
     {
         [Range(10000, 20000)]
+        [Display(Name = "Movie ID")]
         public int MovieId { get; set; }
         [Range(50000, 60000)]
+        [Display(Name = "CinemaHall ID")]
         public int CinemaHallId { get; set; }
+        [Display(Name = "Starting time")]
         public string DateTime { get; set; }
     }
 }
