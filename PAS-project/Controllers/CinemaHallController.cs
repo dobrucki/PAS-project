617417928494a4126c7cc8ea0f9fd5c51d1b6c49
Jedrzej Dobrucki <dobrucki2@gmail.com>
2020-01-06 15:22:29 +0100

    Commit of recent changes

--- PAS-project/Controllers/CinemaHallController.cs
+++ PAS-project/Controllers/CinemaHallController.cs
@@ -1,21 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using PAS_project.Models.Managers;
+using PAS_project.ViewModels;
 
 namespace PAS_project.Controllers
 {
     public class CinemaHallController : Controller
     {
         private readonly CinemaHallManager _cinemaHallManager;
+        private readonly CinemaEventManager _cinemaEventManager;
 
-        public CinemaHallController(CinemaHallManager cinemaHallManager)
+        public CinemaHallController(CinemaHallManager cinemaHallManager, CinemaEventManager cinemaEventManager)
         {
             _cinemaHallManager = cinemaHallManager;
+            _cinemaEventManager = cinemaEventManager;
         }
 
         public IActionResult All()
         {
             var cinemaHalls = _cinemaHallManager.GetAllCinemaHalls();
             return View(cinemaHalls);
+        }
+
+        public IActionResult Details([FromRoute] int? id)
+        {
+            if (id is null) return NotFound();
+            var cinemaHall = _cinemaHallManager.GetCinemaHallById(id.Value);
+            if (cinemaHall is null) return NotFound();
+            return View(cinemaHall);
         }
     }
 }
